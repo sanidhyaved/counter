61053d9e9117f4f9aa3d932258c07d86fa634f64
Sanidhya Ved <sanidhyaved@gmail.com>
2021-08-29 16:30:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ btns.forEach(function(btn) {
         } else {
             count = 0;
         }
+        if(count>0){
+            value.style.color = 'green';
+        } else if(count<0) {
+            value.style.color = 'red';
+        } else {
+            value.style.color = '#222';
+        }
         value.textContent = count;
     });
 });
